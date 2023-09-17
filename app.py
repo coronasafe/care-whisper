cd7441f419df1a8c59e93d67cd22eccd00134af4
ishanExtreme <ishan2198@hotmail.com>
2023-09-17 20:21:14 +0530

    api improved

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ config = dotenv_values(".env")
 BASE_URL = "/api/care-whisper/"
 
 routes = [
-    Route(BASE_URL+"ping", ping, methods=["GET"]),
-    Route(BASE_URL+"health-check", health_check, methods=["GET"]),
-    Route(BASE_URL+"transcribe", transcribe, methods=["POST"]),
+    Route(BASE_URL + "ping", ping, methods=["GET"]),
+    Route(BASE_URL + "health-check", health_check, methods=["GET"]),
+    Route(BASE_URL + "transcribe", transcribe, methods=["POST"]),
 ]
 
 app = Starlette(debug=True, routes=routes)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
--- routes.py
+++ routes.py
@@ -1,34 +1,47 @@
+import uuid
+import json
+
 from starlette.responses import JSONResponse
 from routes_definition.transcribe.transcribe import transcribe_helper
 
 from base import model_small, model_base_en, model_tiny
 from utils.utils import adownload_audio_file
+from base import ROOT_DIR
+
 
 async def ping(request):
     return JSONResponse({"status": 200, "message": "success", "data": {}})
 
+
 async def health_check(request):
     return JSONResponse({"status": 200, "message": "success", "data": {}})
 
-async def transcribe(request):
-    data = await request.json()
 
-    if "inputAudio" not in data:
-          return JSONResponse(status_code=400, content={"status": 400, "message": "Bad Request", "data": {}})
-    
-    # tiny, base.en, small
-    model_name = data.get("model", "tiny")
-    
-    audio_url = data["inputAudio"]
-    file_path = await adownload_audio_file(audio_url)
+async def transcribe(request):
+    form = await request.form()
+
+    audio = form.get("audio")
+    model_name = form.get("model", "tiny")
+    language = form.get("language", "en")
+
+    if audio is None:
+        return JSONResponse(
+            status_code=400,
+            content={"status": 400, "message": "Bad Request", "data": {}},
+        )
+
+    file_path = f"{str(ROOT_DIR)}/audios/{str(uuid.uuid4())}.mp3"
+    with open(file_path, "wb") as f:
+        f.write(audio.file.read())
 
     model = model_tiny
     if model_name == "base.en":
         model = model_base_en
     elif model_name == "small":
         model = model_small
-    
-    res = transcribe_helper(model, audio_path=file_path)
 
-    return JSONResponse({"status": 200, "message": "success", "data": {"transcription": res}})
+    res = transcribe_helper(model, audio_path=file_path, language=language)
 
+    return JSONResponse(
+        {"status": 200, "message": "success", "data": {"transcription": res}}
+    )
